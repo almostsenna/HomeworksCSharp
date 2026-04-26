6868657582fde9018f399ffb4cd1239d884b8cac
almostsenna <olegtomylak@gmail.com>
2026-04-26 15:19:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿//namespace HomeworksCSharp
+//{
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            int number;
+//            Console.Write("Введіть число: ");
+
+//            if (!int.TryParse(Console.ReadLine(), out number))
+//            {
+//                Console.WriteLine("Помилка: введіть коректне число!"); // тут кириллиця норамльно працює, юхууу
+//            }
+//            else if (number < 1 || number > 100)
+//            {
+//                Console.WriteLine("Помилка: число має бути від 1 до 100!");
+//            }
+//            else
+//            {
+//                RunFizzBuzz(number);
+//            }
+
+//            static void RunFizzBuzz(int number)
+//            {
+//                for (int i = 1; i <= number; i++)
+//                {
+//                    if (i % 3 == 0 && i % 5 == 0)
+//                        Console.WriteLine("FizzBuzz");
+//                    else if (i % 3 == 0)
+//                        Console.WriteLine("Fizz");
+//                    else if (i % 5 == 0)
+//                        Console.WriteLine("Buzz");
+//                    else
+//                        Console.WriteLine(i);
+//                }
+//            }
+//        }
+//    }
+//}
---
+++ Task2/Task2.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace HomeworksCSharp.NewFolder
+//{
+//    internal class Task2
+//    {
+//        static void Main()
+//        {
+//            Console.OutputEncoding = Encoding.UTF8;
+//            Console.Write("Введіть число: ");
+//            double number = Convert.ToDouble(Console.ReadLine());
+
+//            Console.Write("Введіть відсоток: ");
+//            double percent = Convert.ToDouble(Console.ReadLine());
+
+//            double result = number * percent / 100;
+
+//            Console.WriteLine($"{percent}% від {number} = {result}");
+//        }
+//    }
+//}
