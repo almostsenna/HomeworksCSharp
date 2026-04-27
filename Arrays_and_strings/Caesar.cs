53ad1ca1e6fbe910298bfeba1482557cd5c26443
almostsenna <olegtomylak@gmail.com>
2026-04-27 12:31:08 +0300

    Second HW - Arrays and Strings

---
+++ Arrays_and_strings/Caesar.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static string alphabet = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ" +
+                             "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя";
+
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.Write("Введіть рядок для шифрування: ");
+        string input = Console.ReadLine();
+
+        Console.Write("Введіть зсув (наприклад, 3): ");
+        int shift = int.Parse(Console.ReadLine());
+
+        string encrypted = CaesarEncrypt(input, shift);
+        string decrypted = CaesarDecrypt(encrypted, shift);
+
+        Console.WriteLine($"\nЗашифрований текст: {encrypted}");
+        Console.WriteLine($"Розшифрований текст: {decrypted}");
+    }
+
+    static string CaesarEncrypt(string text, int shift)
+    {
+        StringBuilder result = new StringBuilder();
+
+        foreach (char c in text)
+        {
+            int index = alphabet.IndexOf(c);
+            if (index == -1)
+            {
+                result.Append(c);
+            }
+            else
+            {
+                int newIndex = (index + shift) % alphabet.Length;
+                result.Append(alphabet[newIndex]);
+            }
+        }
+
+        return result.ToString();
+    }
+
+    static string CaesarDecrypt(string text, int shift)
+    {
+        StringBuilder result = new StringBuilder();
+
+        foreach (char c in text)
+        {
+            int index = alphabet.IndexOf(c);
+            if (index == -1)
+            {
+                result.Append(c);
+            }
+            else
+            {
+                int newIndex = (index - shift + alphabet.Length) % alphabet.Length;
+                result.Append(alphabet[newIndex]);
+            }
+        }
+
+        return result.ToString();
+    }
+}
---
+++ Arrays_and_strings/Program.cs
@@ -0,0 +1,73 @@
+﻿//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+//        int[,] array = new int[5, 5];
+//        Random rand = new Random();
+
+//        for (int i = 0; i < 5; i++)
+//        {
+//            for (int j = 0; j < 5; j++)
+//            {
+//                array[i, j] = rand.Next(-100, 101);
+//                Console.Write($"{array[i, j],5}");
+//            }
+//            Console.WriteLine();
+//        }
+
+//        int min = array[0, 0], max = array[0, 0];
+//        int minIndex = 0, maxIndex = 0;
+//        int index = 0;
+
+//        for (int i = 0; i < 5; i++)
+//        {
+//            for (int j = 0; j < 5; j++)
+//            {
+//                if (array[i, j] < min)
+//                {
+//                    min = array[i, j];
+//                    minIndex = index;
+//                }
+//                if (array[i, j] > max)
+//                {
+//                    max = array[i, j];
+//                    maxIndex = index;
+//                }
+//                index++;
+//            }
+//        }
+
+//        int start, end;
+//        if (minIndex < maxIndex)
+//        {
+//            start = minIndex;
+//            end = maxIndex;
+//        }
+//        else
+//        {
+//            start = maxIndex;
+//            end = minIndex;
+//        }
+//        int sum = 0;
+//        index = 0;
+//        for (int i = 0; i < 5; i++)
+//        {
+//            for (int j = 0; j < 5; j++)
+//            {
+//                if (index > start && index < end)
+//                {
+//                    sum += array[i, j];
+//                }
+//                index++;
+//            }
+//        }
+
+//        Console.WriteLine($"\nМінімальний елемент: {min}");
+//        Console.WriteLine($"Максимальний елемент: {max}");
+//        Console.WriteLine($"Сума елементів між ними: {sum}");
+//    }
+//}
